msiebuhr/charcod.es

commit f9b2fcf461d7697ec6b219d8a05160e2e6449144

Also show the character as a hex code point as used in most programming languages.

http-pub/index.js
@@ -131,6 +131,7 @@ $(document).ready(function () {
             var body = [];
             //body.push("<i>Name:</i> " + info.n);
             body.push("<i>Codepoint:</i> " + codepoint);
+            body.push("<i>Source code:</i> \u" + (codepoint + 0x10000).toString(16).substr(-4));
             body.push("<i>HTML:</i> &amp;" + codepoint + ";");
             if (info.a && info.a.length > 0) {
                 body.push("<i>Alias(es):</i> " + info.a.join(", "));

commit 83daaae501f8b9cb7f7bdcebef84dc5be3d88fd7

Fix escaping-bug introduced in f9b2fcf461.

http-pub/index.js
@@ -131,7 +131,7 @@ $(document).ready(function () {
             var body = [];
             //body.push("<i>Name:</i> " + info.n);
             body.push("<i>Codepoint:</i> " + codepoint);
-            body.push("<i>Source code:</i> \u" + (codepoint + 0x10000).toString(16).substr(-4));
+            body.push("<i>Source code:</i> \\u" + (codepoint + 0x10000).toString(16).substr(-4));
             body.push("<i>HTML:</i> &amp;" + codepoint + ";");
             if (info.a && info.a.length > 0) {
                 body.push("<i>Alias(es):</i> " + info.a.join(", "));