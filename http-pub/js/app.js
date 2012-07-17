msiebuhr/charcod.es

commit aafc910625010b854e2de8c2f9356b7e960c2b65

Removed jQuery.each when parsing data. 620ms

http-pub/js/app.js
@@ -129,20 +129,22 @@
             dataType: 'json',
             success: function (data) {
                 console.profile();
-                $.each(data, function (index, unicodeChar) {
-                    unicodeTable[unicodeChar.c] = unicodeChar;
+                var i = 0,
+                    len = data.length,
+                    unicodeChar;
+
+                for (; i < len; i++) {
+                    unicodeChar = data[i];
 
                     // Use name as tags
                     tagsToTrigrams(unicodeChar.c, unicodeChar.n.split(" "));
-
                     // Then the block
                     tagsToTrigrams(unicodeChar.c, [unicodeChar.b]);
-
                     // Use alias as tags
                     if (unicodeChar.a) {
                         tagsToTrigrams(unicodeChar.c, unicodeChar.a);
                     }
-                });
+                }
                 console.profileEnd();
 
                 // Search right away if the user has already typed something

commit b7546788cd6445042176ad94a0ea5a804f40cbf2

Remember to store the raw unicode data somewhere.
Fixes bug introduced in aafc9106 where codepoint name and group wouldn't
be shown in the details.

http-pub/js/app.js
@@ -134,6 +134,7 @@
 
                 for (; i < len; i++) {
                     unicodeChar = data[i];
+                    unicodeTable[unicodeChar.c] = unicodeChar;
 
                     // Use name as tags
                     tagsToTrigrams(unicodeChar.c, unicodeChar.n.split(" "));