0a5a9abb5e527ada2e452081765b93896ecfb855
Morten Siebuhr <sbhr@sbhr.dk>
2015-09-25 14:35:12 +0200

    Clipboard: Track when someone copies something.

--- http-pub/js/app.js
+++ http-pub/js/app.js
@@ -117,6 +117,7 @@ require(['search'], function (Search) {
                         // Activate clipboard button
                         var btn = tpl.find('.copyButton')[0];
                         if (btn) {
+                            _gaq.push(['_trackEvent', 'copy-button', info.b]);
                             btn.setAttribute('data-clipboard-text', String.fromCodePoint(codePoint));
                             cpb = clipboardButton(btn);
                         }
